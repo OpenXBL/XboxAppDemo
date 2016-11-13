9d0b2a8f7eda1c96e925b17fe9197536ec467b28
David Regimbal <regimbal93@gmail.com>
2016-11-13 18:37:54 -0500

    Update index.php

--- index.php
+++ index.php
@@ -55,5 +55,6 @@ if($_POST['a'])
       </form>
     </div>
   </body>
+  <p style="position: absolute; bottom: 0; left: 0; border: 0; padding: 4px; font-size: 7pt;">We are in no way endorsed or affiliated with the Microsoft Corporation, Xbox, Xbox LIVE or any Microsoft subsidiary. Images are registered trademarks of their respected owners.</p>
   <a href="https://github.com/OpenXBL/Demo"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/e7bbb0521b397edbd5fe43e7f760759336b5e05f/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f677265656e5f3030373230302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_green_007200.png"></a>
 </html>
